0bbe300862a5c59e6f373f8e5a286c9295e3d89d
Evgeny Levchenya <levchenya.evgeny@gmail.com>
2026-01-17 09:32:05 +0100

    bottom up fibanacci

--- src/tasks/fibonacci.test.ts
+++ src/tasks/fibonacci.test.ts
@@ -1,21 +1,33 @@
-import { describe, it } from 'node:test';
-import assert from 'node:assert/strict';
+import { describe, it } from "node:test";
+import assert from "node:assert/strict";
 
-import { getFibonacciNumber, getFibonacciSequence } from './fibonacci.ts';
+import {
+  getFibonacciNumberTopDown,
+  getFibonacciNumberBottomUp,
+  getFibonacciSequence,
+} from "./fibonacci.ts";
 
-describe('getFibonacciNumber', () => {
-    it('should return Fibonacci number', () => {
-        // 0 1 1 2 3 5 8 13 21 34 55 89
+describe("getFibonacciNumberTopDown", () => {
+  it("should return Fibonacci number", () => {
+    // 0 1 1 2 3 5 8 13 21 34 55 89
 
-        assert.equal(getFibonacciNumber(10, new Map()), 55)
-    })
-})
+    assert.equal(getFibonacciNumberTopDown(10, new Map()), 55);
+  });
+});
 
-describe('getFibonacciSequence', () => {
-    it('should return sequence', () => {
-        const expectedResult = [0, 1, 1, 2, 3, 5, 8, 13, 21]
-        const number = 8
+describe("getFibonacciNumberBottomUp", () => {
+  it("should return Fibonacci number", () => {
+    // 0 1 1 2 3 5 8 13 21 34 55 89
 
-        assert.deepEqual(getFibonacciSequence(number), expectedResult)
-    })
-})
+    assert.equal(getFibonacciNumberBottomUp(10), 55);
+  });
+});
+
+describe("getFibonacciSequence", () => {
+  it("should return sequence", () => {
+    const expectedResult = [0, 1, 1, 2, 3, 5, 8, 13, 21];
+    const number = 8;
+
+    assert.deepEqual(getFibonacciSequence(number), expectedResult);
+  });
+});
--- src/tasks/fibonacci.ts
+++ src/tasks/fibonacci.ts
@@ -1,25 +1,48 @@
-export const getFibonacciNumber = (number: number, memo: Map<number, number>): number => {
-    if (number < 2) {
-        return number
-    }
+export const getFibonacciNumberTopDown = (
+  number: number,
+  memo: Map<number, number>,
+): number => {
+  if (number < 2) {
+    return number;
+  }
 
-    if (memo.has(number)) {
-        return memo.get(number) as number
-    }
+  if (memo.has(number)) {
+    return memo.get(number) as number;
+  }
 
-    const result = getFibonacciNumber(number - 1, memo) + getFibonacciNumber(number - 2, memo)
+  const result =
+    getFibonacciNumberTopDown(number - 1, memo) +
+    getFibonacciNumberTopDown(number - 2, memo);
 
-    memo.set(number, result)
+  memo.set(number, result);
 
-    return result
-}
+  return result;
+};
+
+export const getFibonacciNumberBottomUp = (number: number): number => {
+  const numbers = [0, 1];
+
+  if (number === 1) {
+    return numbers[0];
+  }
+
+  if (number === 2) {
+    return numbers[1];
+  }
+
+  for (let i = 2; i <= number; i++) {
+    numbers.push(numbers[i - 1] + numbers[i - 2]);
+  }
+
+  return numbers[numbers.length - 1];
+};
 
 export const getFibonacciSequence = (number: number): number[] => {
-    const result = [0, 1]
+  const result = [0, 1];
 
-    for (let i = 2; i <= number; i++) {
-        result.push(result[i - 1] + result[i - 2])
-    }
- 
-    return result
-}
+  for (let i = 2; i <= number; i++) {
+    result.push(result[i - 1] + result[i - 2]);
+  }
+
+  return result;
+};
